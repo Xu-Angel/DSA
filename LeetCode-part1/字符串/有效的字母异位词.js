3bcec75c3b87858f857f5115f59105bfab2b1a6d
Xu-Angel <1049547382@qq.com>
2019-05-20 22:14:32 +0800

    有效的字母异位词

---
+++ LeetCode-part1/字符串/有效的字母异位词.js
@@ -0,0 +1,18 @@
+/* 1. 利用替换删除字符
+
+思路：
+
+*   循环第一个字符串，将第二个字符串中替换当前字符为空，即，如果当前字符在第二个字符串中存在，则删除，不存在，则什么也不干
+
+*   循环结束后，如果第二个字符串中还有字符，则不是异位词，返回 false
+*   否则返回 true */
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false
+  for (let i = 0; i < s.length; i++) {
+    t = t.replace(s[i], '')
+  }
+  if (t.length !== 0) return false
+  return true
+}
+
+// 如果存在 Unicode 字符，利用正则，将其替换正常的字符（使用String.fromCodePoint()）
